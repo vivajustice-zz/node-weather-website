9c827d0da153c19f1e80a351d758d336365a73c2
meth0d <sam.tunnicliff@gmail.com>
2020-05-01 12:49:49 +0100

    new weather information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find weather report for that location, try again', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] +'. It is currently ' + body.current.temperature + ' out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] +'. It is currently ' + body.current.temperature + ' out. There is a ' + body.current.precip + '% chance of rain. The wind is blowing ' + body.current.wind_dir + ' at a speed of ' + body.current.wind_speed + 'mph. Pressure currently is ' + body.current.pressure + ' bar' )
             
             }
     })
